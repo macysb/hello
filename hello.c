264f0697e222f633c097ed0be4971bea56352e3e
Macy Sweet-Bemer <msweet-bemer2@unl.edu>
2025-08-28 18:04:46 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Macy Sweet-Bemer
+ *         msweet-bemer2@unl.edu
+ * Date: 2025/08/28
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Macy Sweet-Bemer\n");
+
+  return 0;
+}
